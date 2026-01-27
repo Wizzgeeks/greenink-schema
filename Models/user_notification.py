03561561b75055a582b18976a73fac87083ec318
Rajasekar <rajasekar@wizzgeeks.com>
2026-01-27 17:38:00 +0530

    fixed notification name feild

--- Models/user_notification.py
+++ Models/user_notification.py
@@ -6,6 +6,7 @@ import uuid
 class Notification(EmbeddedDocument):
     notification_id = StringField(default=lambda: str(uuid.uuid4()))
     message = StringField(default="You have a new notification")
+    name=StringField(default="")
     notification_type = StringField(
         choices=("homework", "reminder", "general","test"),
         default="general"
@@ -17,6 +18,7 @@ class Notification(EmbeddedDocument):
         return {
             "notification_id": self.notification_id,
             "message": self.message,
+            "name":self.name,
             "notification_type": self.notification_type,
             "mark_as_read": self.mark_as_read,
             "created_at": self.created_at
