a6b9a4844efc5538292bb104823a32a07aa8df34
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2026-02-20 12:33:23 +0530

    add coin_value_per_unit field to Coin_management model

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="greenink-schema",
-    version="1.9.9",
+    version="2.1.0",
     description="Schema for Flask Admin and User backends",
     author="wizzgeeks",
     author_email="",
