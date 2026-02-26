35ba972c3591ffd09708f50ffb0861a56d21e0b3
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2026-02-26 16:49:59 +0530

    add 'materials' option to page_type field in CoursePageCompleted, SubjectPageCompleted, SubtopicPageCompleted, and TopicPageCompleted models

--- Models/course_page_completed.py
+++ Models/course_page_completed.py
@@ -10,7 +10,7 @@ class CoursePageCompleted(Document):
     user = ReferenceField(Users, reverse_delete_rule=CASCADE, required=True)
     completed = BooleanField(default=False)
     hierarcy_level = IntField(default=0)
-    page_type = StringField(choices=['content','quiz','question_bank','test','mcq','match','fillups','content','expand','update','trueorfalse','analysis'], required=True)
+    page_type = StringField(choices=['content','quiz','question_bank','test','mcq','match','fillups','content','expand','update','trueorfalse','analysis','materials'], required=True)
     created_at = DateTimeField(default=lambda: datetime.now(timezone.utc))
     updated_at = DateTimeField(default=lambda: datetime.now(timezone.utc))
 
