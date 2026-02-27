84fd49b557d7ab04354501a7208d4831f1190085
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2026-02-27 10:23:12 +0530

    add Notification and GlobalNotification models for handling notifications

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="greenink-schema",
-    version="2.1.8",
+    version="2.1.9",
     description="Schema for Flask Admin and User backends",
     author="wizzgeeks",
     author_email="",
