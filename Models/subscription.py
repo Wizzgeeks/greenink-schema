5d052aea9e5900879d54ddedef2bd8386616c95e
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2026-03-02 12:37:45 +0530

    add User_subscription model to manage user subscriptions and related data

--- Models/subscription.py
+++ Models/subscription.py
@@ -1,13 +1,29 @@
-from mongoengine import Document,StringField,ReferenceField,BooleanField,DateTimeField,FloatField,DictField,CASCADE
+from mongoengine import Document,StringField,ReferenceField,BooleanField,DateTimeField,FloatField,IntField,ListField,EmbeddedDocument,EmbeddedDocumentField,CASCADE,DictField
 from Models.course import Course
 from datetime import datetime, timezone
 
+
+class Permission(EmbeddedDocument):
+    name = StringField(required=True)
+    active = BooleanField(default=True)
+
+    def to_json(self):
+        return {
+            "name": self.name,
+            "active": self.active
+        }
+
+
 class Subscription(Document):
     course = ReferenceField(Course,reverse_delete_rule=CASCADE,required=True)
-    term_in_months = StringField(required=True)
-    price = FloatField(required=True)
+    
     package_name = StringField(required=True)
-    plan_active = BooleanField(default=True)
+    plan_type = StringField(required=True, choices=("week", "month", "year"))
+    subscription_tier=StringField(required=True)
+    price = FloatField(required=True)
+    plan_duration=IntField(required=True)
+    activate = BooleanField(default=True)
+    premissions = ListField(EmbeddedDocumentField(Permission))
     created_at = DateTimeField(default=lambda: datetime.now(timezone.utc))
     updated_at = DateTimeField(default=lambda: datetime.now(timezone.utc))
 
@@ -15,29 +31,38 @@ class Subscription(Document):
         return {
             "id": str(self.id),
             "course":str(self.course.id) if self.course else None,
-            "term_in_months":self.term_in_months,
+            "package_name":self.package_name,
+            "plan_type":self.plan_type,
+            "subscription_tier":self.subscription_tier,
             "price":self.price,
-            "package_name":self.package_name
+            "plan_duration":self.plan_duration,
+            "activate":self.activate,
+            "premissions":[p.to_json() for p in self.premissions] if self.premissions else []
         }
     
     def with_key(self):
         return {
             "id": str(self.id),
             "course":self.course.to_json() if self.course else None,
-            "term_in_months":self.term_in_months,
-            "price":self.price,
             "package_name":self.package_name,
-            "plan_active":self.plan_active
-
-                }
+            "plan_type":self.plan_type,
+            "subscription_tier":self.subscription_tier,
+            "price":self.price,
+            "plan_duration":self.plan_duration,
+            "activate":self.activate,
+            "premissions":[p.to_json() for p in self.premissions] if self.premissions else []
+        }
     def admin_json(self):
         return {
             "id": str(self.id),
             "course":self.course.to_json() if self.course else None,
-            "term_in_months":self.term_in_months,
-            "price":self.price,
             "package_name":self.package_name,
-            "plan_active":self.plan_active
+            "plan_type":self.plan_type,
+            "subscription_tier":self.subscription_tier,
+            "price":self.price,
+            "plan_duration":self.plan_duration,
+            "activate":self.activate,
+            "premissions":[p.to_json() for p in self.premissions] if self.premissions else []
         }   
     
     
---
+++ Models/user_subscription.py
@@ -0,0 +1,38 @@
+from mongoengine import Document,IntField,ReferenceField,DateTimeField,CASCADE
+# from Models.coupon_model import Coupon
+from Models.subscription import Subscription
+from Models.user import Users
+from datetime import datetime, timezone
+
+
+class User_subscription(Document):
+    user = ReferenceField(Users,reverse_delete_rule=CASCADE,required=True)
+    subscription = ReferenceField(Subscription,reverse_delete_rule=CASCADE,required=True)
+    # coupon = ReferenceField(Coupon,reverse_delete_rule=CASCADE)
+    coins_redeemed=IntField()
+    expiry = DateTimeField(required=True)
+    created_at = DateTimeField(default=lambda: datetime.now(timezone.utc))
+    updated_at = DateTimeField(default=lambda: datetime.now(timezone.utc))
+    
+
+    def to_json(self):
+        return {
+            "id":str(self.id),
+            "user":str(self.user.id) if self.user else None,
+            "subscription":self.subscription.to_json() if self.subscription else None,
+            "coupon":str(self.coupon.id) if self.coupon else None,
+            "coins_redeemed":self.coins_redeemed if self.coins_redeemed else None,
+            "expiry":self.expiry,
+            "created_at":self.created_at,
+            "updated_at":self.updated_at
+        }
+    
+    # def with_key(self):
+    #     return {
+    #         "id":str(self.id),
+    #         "user":self.user.to_json() if self.user else None,
+    #         "subscription":self.subscription.to_json() if self.subscription else None,
+    #         "coupon":self.coupon.to_json() if self.coupon else None,
+    #         "coins_redeemed":self.coins_redeemed if self.coins_redeemed else None,
+    #         "expiry":self.expiry.strftime('%d/%m/%Y')
+    #     }
